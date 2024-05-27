32ba5f39d5b57b1b241f6ac2f8303afe719b7df7
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-05-27 23:12:14 +0530

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [sum, setSum] = useState(0);
 
-export default App
+  const addSum = (value) => {
+    const number = Number(value);
+    if (!isNaN(number)) {
+      setSum(number + sum);
+    } else {
+      setSum(0);
+    }
+  };
+
+  useEffect(() => {}, [sum]);
+
+  return (
+    <div className="main">
+      {/* Do not remove the main div */}
+      <h1>Sum Calculator</h1>
+      <br />
+      <input
+        type="number"
+        name="num"
+        id="num"
+        onChange={(e) => addSum(e.target.value)}
+      />
+      <br />
+      <p>Sum : {sum}</p>
+    </div>
+  );
+};
+
+export default App;
